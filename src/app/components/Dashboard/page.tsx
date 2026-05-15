898d63f579675c366c5967c0f15038c8ad759e2c
leeron ogan-solomon <leeron.ogan-solomon@pau.edu.ng>
2026-05-15 17:24:14 +0100

    fix: remove user profile photo container for repositioning

--- src/app/components/Dashboard/page.tsx
+++ src/app/components/Dashboard/page.tsx
@@ -1,15 +1,9 @@
-
-
 const Dashboard = () => {
     return (
-        <div style={{display:'flex' , justifyContent:'space-between', alignItems: 'center', paddingTop:'90px'}}>
+        <div style={{display:'flex', justifyContent:'space-between', alignItems: 'center', paddingTop:'30px', paddingLeft:'32px', paddingRight:'50px'}}>
             <div>
                 <h1 style={{fontSize:'28px', fontWeight: 'bold', color: '#111827'}}>Welcome back, user</h1>
-                <p style={{fontSize: '13px', color: '#9ca3af', marginTop: '3px'}}>Here is your overveiw for october</p>
-            </div>
-
-            <div style = {{width:'30px', height:'30px',borderRadius: '100px', backgroundColor:'#a1a1a1', display: 'flex', alignItems:'center', paddingLeft:'30px'}}>
-                <p></p>
+                <p style={{fontSize: '13px', color: '#9ca3af', marginTop: '3px'}}>Here is your overview for October</p>
             </div>
         </div>
     );
