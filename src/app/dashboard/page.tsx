a0b0a1b5652706c35f169cf390d43d94874e6645
leeron ogan-solomon <leeron.ogan-solomon@pau.edu.ng>
2026-05-20 15:24:33 +0100

    feat: Add budget limit to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -36,6 +36,14 @@ const Dashboard = () => {
                 <p style={{fontSize: '13px', color: '#9ca3af', marginTop: '3px'}}>
                     Here is your overview for {currentMonth}
                 </p>
+
+                 <div style={{ padding: '36px', backgroundColor: '#E8F5F3', borderRadius: '15px', width: '100%', boxSizing: 'border-box', marginTop: '30px'}}>
+                    <p style={{ fontSize: '15px', fontWeight: '700', color: '#0d4d4d', margin: 0 }}>BUDGET LIMIT</p>
+                    <div style={{ backgroundColor: '#a8cec1', borderRadius: '999px', height: '6px', marginTop: '8px', marginRight:'30px'}}>
+                        <div style={{ backgroundColor: '#0d4d4d', borderRadius: '999px', height: '6px', width: '75%' }}></div>
+                    </div>
+                    <p style={{ fontSize: '11px', color: '#0d4d4d', marginTop: '8px', margin:'2px'}}>75% of monthly limit used</p>
+                </div>
             </div>
         </div>
     );
