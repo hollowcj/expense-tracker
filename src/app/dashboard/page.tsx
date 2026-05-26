3e7997c15c60c63ede74368968dcdd6825be5f1a
leeron ogan-solomon <leeron.ogan-solomon@pau.edu.ng>
2026-05-26 19:51:52 +0100

    feat: add top bar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '@/lib/supabase';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
-import { Pointer, View, ArrowUpCircleIcon} from 'lucide-react';
+import { Pointer, View, ArrowUpCircleIcon, TrendingUp, TrendingDown} from 'lucide-react';
 
 
 const Dashboard = () => {
@@ -51,7 +51,15 @@ const weeklyData = [
 ];
 
     return (
-    <div style={{paddingTop:'30px', paddingLeft:'32px', paddingRight:'50px'}}>
+           
+        
+    <div style={{paddingTop:'0px',paddingBottom:'20px', paddingLeft:'15px', paddingRight:'30px'}}>
+         <div style={{backgroundColor:'#E8F5F3', width:'100%',height:'60px', marginBottom:'6px',borderRadius:'100px',display:'flex',alignItems:'center', paddingRight:'12px', justifyContent:'flex-end'}}>
+                <div style={{width:'44px', height:'44px', borderRadius:'50%', backgroundColor:'#0d4d4d', display:'flex', alignItems:'center', justifyContent:'center', cursor:'pointer'}}>
+                    <p style={{color:'#E8F5F3', fontWeight:'bold', fontSize:'16px', margin:'0'}}>L</p>
+                </div>
+            
+            </div>
         
         {/* Welcome Row */}
         <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
@@ -63,24 +71,24 @@ const weeklyData = [
                     Here is your overview for {currentMonth}
                 </p>
             </div>
-            <div style={{marginBottom:'30px'}}>
-            <div style={{width:'44px', height:'44px', borderRadius:'50%', backgroundColor:'#0d4d4d', display:'flex', alignItems:'center', justifyContent:'center', cursor:'pointer'}}>
-                <p style={{color:'#E8F5F3', fontWeight:'bold', fontSize:'16px', margin:'0'}}>L</p>
-            </div>
-            </div>
         </div>
 
         {/* 3 Cards Row */}
             <div style={{display:'grid', gridTemplateColumns:'1fr 1fr 1fr 1fr', gap:'20px', marginTop:'30px'}}>
 
                 <div style={{padding:'24px', backgroundColor:'#0d4d4d', borderRadius:'15px'}}>
-                <p style={{fontSize:'17px', fontWeight:'500', color:'#E8F5F3'}}>BALANCE</p>
+                <p style={{fontSize:'18px', fontWeight:'500', color:'#E8F5F3'}}>Balance</p>
                 <p style={{fontSize:'33px', fontWeight:'bolder', color:'#E8F5F3', marginTop:'50px'}}>₦148,200.00</p>
-                <p style={{fontSize:'12px', color:'#E8F5F3', marginTop:'0px'}}>On track</p>
+                    <div style={{display:'flex',gap:'5px', alignItems:'center'}}>
+                        <div style={{display:'flex', alignItems:'center',gap:'5px',border:'2px solid #E8F5F3', borderRadius:'800px', paddingRight:'10px',paddingLeft:'10px',paddingTop:'3px', paddingBottom:'3px', marginTop:'5px'}}>
+                            <p style={{fontSize:'12px', color:'#E8F5F3', marginTop:'0px'}}>On track</p>
+                            <TrendingUp size={20} color='#E8F5F3'/> 
+                        </div>
+                    </div>
                 </div>
                 
                 <div style={{padding:'24px', backgroundColor:'#E8F5F3', borderRadius:'15px'}}>
-                    <p style={{fontSize:'17px', fontWeight:'500', color:'#0d4d4d'}}>BUDGET USAGE</p>
+                    <p style={{fontSize:'18px', fontWeight:'500', color:'#0d4d4d'}}>Budget Usage</p>
 
                         <div style={{marginTop:'24px'}}>
                             <p style={{fontSize:'11px', color:'#0d4d4d', marginTop:'8px'}}>Food</p>
@@ -107,22 +115,31 @@ const weeklyData = [
 
 
             <div style={{padding:'24px', backgroundColor:'#E8F5F3', borderRadius:'15px'}}>
-                <p style={{fontSize:'17px', fontWeight:'500', color:'#0d4d4d'}}>MONTHLY INCOME</p>
+                <p style={{fontSize:'18px', fontWeight:'500', color:'#0d4d4d'}}>Monthly Income</p>
                 <p style={{fontSize:'35px', fontWeight:'bolder', color:'#0d4d4d', marginTop:'50px'}}>₦148,200.00</p>
-                <p style={{fontSize:'12px', color:'#0d4d4d', marginTop:'0px'}}>On track</p>
+
+                <div style={{display:'flex',gap:'5px', alignItems:'center'}}>
+                    <div style={{display:'flex', alignItems:'center',gap:'5px',border:'2px solid #0d4d4d', borderRadius:'800px', paddingRight:'10px',paddingLeft:'10px',paddingTop:'3px', paddingBottom:'3px', marginTop:'5px'}}>
+                        <p style={{fontSize:'12px', color:'#0d4d4d', marginTop:'0px'}}>On track</p>
+                        <TrendingUp size={20} color='#0d4d4d'/> 
+                    </div>
                 </div>
+            </div>
 
             <div style={{padding:'24px', backgroundColor:'#E8F5F3', borderRadius:'15px'}}>
-                <p style={{fontSize:'17px', fontWeight:'500', color:'#0d4d4d'}}>MONTHLY EXPENSES</p>
+                <p style={{fontSize:'18px', fontWeight:'500', color:'#0d4d4d'}}>Monthly Expenses</p>
                 <p style={{fontSize:'35px', fontWeight:'bolder', color:'#0d4d4d', marginTop:'50px'}}>₦134,120.45</p>
-                <p style={{fontSize:'12px', color:'#0d4d4d', marginTop:'0px'}}>12% higher than usual</p>
+                <div style={{display:'flex',gap:'5px', alignItems:'center', border:'2px solid #0d4d4d', borderRadius:'800px', paddingLeft:'7px',paddingTop:'3px', paddingBottom:'3px', marginTop:'5px'}}>
+                <p style={{fontSize:'12px', color:'#0d4d4d', marginTop:'0px'}}>12% Percent higher than usual</p>
+                <TrendingDown size={20} color='#0d4d4d'/> 
+                </div>
             </div>
         </div>
 
         {/* Spending Trends Card */}
         <div style={{padding:'24px', backgroundColor:'#E8F5F3', borderRadius:'15px', marginTop:'30px', minHeight:'300px'}}>
             <div style={{display:'flex', justifyContent:'space-between', alignItems:'center', marginBottom:'10px'}}>
-                <p style={{fontSize:'13px', fontWeight:'700', color:'#0d4d4d'}}>SPENDING TRENDS</p>
+                <p style={{fontSize:'17px', fontWeight:'700', color:'#0d4d4d'}}>Spending Trends</p>
                     <div style={{display:'flex', gap:'8px', marginRight:'30px'}}>
                         <button onClick={() => setView("weekly")} style={{padding:'6px 12px', borderRadius:'8px', fontSize:'12px', border:'1px solid #0d4d4d', cursor:'pointer', backgroundColor: view === "weekly" ? '#0d4d4d' : 'transparent', color: view === "weekly" ? 'white' : '#0d4d4d'}}>
                             Weekly
@@ -144,12 +161,35 @@ const weeklyData = [
                 </div>     
             </div>
 
-            <div style={{padding:'24px', backgroundColor:'#E8F5F3', borderRadius:'15px', marginTop:'30px', minHeight:'500px'}}>
-                <div style={{display:'grid', gridTemplateColumns:'2fr 1fr 1fr 1fr', gap:'10px', paddingBottom:'10px'}}>
+            <div style={{padding:'24px', backgroundColor:'#E8F5F3', borderRadius:'15px', marginTop:'30px', minHeight:'250px'}}>
+                <p style={{fontSize:'17px', fontWeight:'700', color:'#0d4d4d'}}>Recent transactions</p>
+                
+                <div style={{display:'grid', gridTemplateColumns:'2fr 1fr 1fr 1fr', gap:'10px', paddingBottom:'10px',marginTop:'20px'}}>
                     <p style={{fontSize:'12px', fontWeight:'700', color:'#9ca3af'}}>TRANSACTION</p>
                     <p style={{fontSize:'12px', fontWeight:'700', color:'#9ca3af'}}>CATEGORY</p>
                     <p style={{fontSize:'12px', fontWeight:'700', color:'#9ca3af'}}>DATE</p>
                     <p style={{fontSize:'12px', fontWeight:'700', color:'#9ca3af'}}>AMOUNT</p>
+                </div>
+
+                <div style={{display:'grid', gridTemplateColumns:'2fr 1fr 1fr 1fr', gap:'10px', paddingTop:'12px', paddingBottom:'12px'}}>
+                    <p style={{fontSize:'13px', color:'#111827'}}>Blue Bottle Coffee</p>
+                    <p style={{fontSize:'13px', color:'#111827'}}>Dining Out</p>
+                    <p style={{fontSize:'13px', color:'#111827'}}>May 1, 2025</p>
+                    <p style={{fontSize:'13px', color:'#e53e3e'}}>-₦12.50</p>
+                </div>
+
+                 <div style={{display:'grid', gridTemplateColumns:'2fr 1fr 1fr 1fr', gap:'10px', paddingTop:'12px', paddingBottom:'12px'}}>
+                    <p style={{fontSize:'13px', color:'#111827'}}>Blue Bottle Coffee</p>
+                    <p style={{fontSize:'13px', color:'#111827'}}>Dining Out</p>
+                    <p style={{fontSize:'13px', color:'#111827'}}>May 1, 2025</p>
+                    <p style={{fontSize:'13px', color:'#e53e3e'}}>-₦12.50</p>
+                </div>
+
+                 <div style={{display:'grid', gridTemplateColumns:'2fr 1fr 1fr 1fr', gap:'10px', paddingTop:'12px', paddingBottom:'12px'}}>
+                    <p style={{fontSize:'13px', color:'#111827'}}>Blue Bottle Coffee</p>
+                    <p style={{fontSize:'13px', color:'#111827'}}>Dining Out</p>
+                    <p style={{fontSize:'13px', color:'#111827'}}>May 1, 2025</p>
+                    <p style={{fontSize:'13px', color:'#e53e3e'}}>-₦12.50</p>
                 </div>
             </div>    
         </div>
